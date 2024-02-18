50f2bd540ae986be6ef8106d75cfb915f7d23945
imran0249 <imrannisho400@gmail.com>
2024-02-19 01:57:42 +0600

    second commit

---
+++ declare.js
@@ -0,0 +1,12 @@
+const pen = { brand: 'econo', price: 10, color: 'black' }
+// console.log(pen);
+
+
+ const pencil = new Object( )
+ console.log(pencil)
+
+
+// const rubber = Object.create({})
+// console.log(rubber)
+
+ 
